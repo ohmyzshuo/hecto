ad608cd38918eeed70d8422a37b5e9aac8efc02b
Shawn Gao Zishuo <li-shuo@live.com>
2024-02-21 00:52:58 +0800

    project initialization

---
+++ build/debug/deps/hecto-9c16d270ef832a1a.d
@@ -0,0 +1,5 @@
+/Users/shawn/RustProjects/hecto/build/debug/deps/libhecto-9c16d270ef832a1a.rmeta: src/main.rs
+
+/Users/shawn/RustProjects/hecto/build/debug/deps/hecto-9c16d270ef832a1a.d: src/main.rs
+
+src/main.rs:
---
+++ build/debug/deps/hecto-a4fba8094c9de65c.d
@@ -0,0 +1,5 @@
+/Users/shawn/RustProjects/hecto/build/debug/deps/libhecto-a4fba8094c9de65c.rmeta: src/main.rs
+
+/Users/shawn/RustProjects/hecto/build/debug/deps/hecto-a4fba8094c9de65c.d: src/main.rs
+
+src/main.rs:
